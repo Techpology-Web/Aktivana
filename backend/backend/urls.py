b35e8d933cade413da481f3697ca305989c6cb34
spynetS <alfred@stensatter.se>
2022-11-04 20:42:06 +0100

    added path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from aktivana.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('company/add/', addCompany),
+
 ]
