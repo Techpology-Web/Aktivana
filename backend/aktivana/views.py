4e587f617d58028d6decf4b16dfc4ced3c99d35f
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-11-04 21:01:55 +0100

    Connection successful

--- backend/aktivana/views.py
+++ backend/aktivana/views.py
@@ -3,5 +3,4 @@ from backend.utils import extractRequest
 
 # Create your views here.
 def testConn(request):
-    req = extractRequest(request)
     return HttpResponse(200)
--- frontend/Src/HomeScreen.js
+++ frontend/Src/HomeScreen.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {
+	SafeAreaView,
+	SafeAreaProvider,
+	SafeAreaInsetsContext,
+	useSafeAreaInsets,
+	initialWindowMetrics,
+} from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
