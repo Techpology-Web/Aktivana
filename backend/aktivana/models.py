e9077dc47cc280f063c2b8d91636c2b55dd641bf
spynetS <alfred@stensatter.se>
2022-11-04 18:38:52 +0100

    #8 added comments

--- backend/aktivana/models.py
+++ backend/aktivana/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Partner (models.Model):
-
-    name    = models.TextField()
-    logo    = models.TextField()
-    phone   = models.TextField()
-    email   = models.TextField()
-    website = models.TextField()
-    adress  = models.TextField()
+#
+    name    = models.TextField() # partner name
+    logo    = models.TextField() # their logo
+    phone   = models.TextField() # their phonenumber
+    email   = models.TextField() # their support email (email a employee can contact)
+    website = models.TextField() # thier website
+    adress  = models.TextField() # thier adress
