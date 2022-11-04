7cfb811203e2479ed66bfe72d991a889243ecf23
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-11-04 20:52:06 +0100

    Testing connection

--- backend/aktivana/views.py
+++ backend/aktivana/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from backend.utils import extractRequest
 
 # Create your views here.
+def testConn(request):
+    req = extractRequest(request)
+    return HttpResponse(200)
