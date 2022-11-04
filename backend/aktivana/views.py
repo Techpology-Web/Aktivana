184005bc199aba4b9dc1ad44b9f63e0cbf171bb8
spynetS <alfred@stensatter.se>
2022-11-04 21:09:57 +0100

    added company with failsafe

--- backend/aktivana/views.py
+++ backend/aktivana/views.py
@@ -3,14 +3,22 @@ from backend.utils import extractRequest, encrypt
 from aktivana.models import Company
 from django.http import HttpResponse
 # Create your views here.
+import json
 
 def addCompany(request):
     req = extractRequest(request)
-    newCompany = Company(
-        email=req["email"],
-        password=encrypt(req["password"]),
-    )
-    newCompany.save()
+    try:
+        if len(Company.objects.filter(email=req["email"])) == 0:
+            newCompany = Company(
+                email=req["email"],
+                password=encrypt(req["password"]),
+            )
+            newCompany.save()
+            return HttpResponse(str(newCompany.__dict__).replace("'",'"'),status=200)
+        return HttpResponse("Company with this email already exist",status=409)
+            
+    except Exception as e:
+        return HttpResponse(e.__str__(),status=400)    
 
 # Create your views here.
 def testConn(request):
