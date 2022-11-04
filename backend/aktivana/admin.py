0c4dcb11f45bdfe3712beb01e13344699ed1ba56
spynetS <alfred@stensatter.se>
2022-11-04 19:26:50 +0100

    #7 #6 #10 added models

--- backend/aktivana/admin.py
+++ backend/aktivana/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from aktivana.models import *
 
 # Register your models here.
+
+admin.register(Code)
+admin.register(Partner)
+admin.register(Company)
+admin.register(Employee)
--- backend/aktivana/models.py
+++ backend/aktivana/models.py
@@ -3,10 +3,42 @@ from django.db import models
 # Create your models here.
 
 class Partner (models.Model):
-#
+
     name    = models.TextField() # partner name
     logo    = models.TextField() # their logo
     phone   = models.TextField() # their phonenumber
     email   = models.TextField() # their support email (email a employee can contact)
     website = models.TextField() # thier website
     adress  = models.TextField() # thier adress
+
+class Code (models.Model):
+    import time
+    expiretime = models.IntegerField(default=0) # unix timestamp
+    useTime    = models.IntegerField(default=1) # how many times it can be used 
+    partner    = models.ForeignKey(Partner, on_delete=models.CASCADE)
+    picture    = models.TextField()             # how the code should look and also what it gives
+
+
+class Company (models.Model):
+    email       = models.TextField()
+    password    = models.TextField()
+    activeCodes = models.ManyToManyField(Code)
+    signupCode  = models.TextField(default=self.createSignupCode())
+
+
+    def createSignupCode(self):
+        import random
+        alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,c"
+        code = ""
+        for i in range(9):
+            code += alphabet[random.randint(0, len(alphabet))]
+        return code
+
+class Employee (models.Model):
+
+    firstName = models.TextField()          
+    lastName  = models.TextField()          
+    password  = models.TextField()          
+    email     = models.TextField()          
+    company   = models.ForeignKey(Company,on_delete=models.CASCADE)  # the company they belong to
+    usedCodes = models.TextField()                                   # the codes they have already used
