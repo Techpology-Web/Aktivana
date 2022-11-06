75a70813aa1498f147df511cda48384843edc505
spynetS <alfred@stensatter.se>
2022-11-06 18:03:44 +0100

    added keyboard type to textinputfield

--- frontend/Src/Components/TextInputField.js
+++ frontend/Src/Components/TextInputField.js
@@ -20,7 +20,15 @@ export default function TextInputField(props) {
 
     return (
 		<View style={[t.border,t.pR3, t.bgWhite, t.roundedLg,t.h12,t.justifyCenter,t.itemsCenter,t.mY3,t.flex,t.flexRow,t.border0,style]} >
-			<TextInput secureTextEntry={showText} onChangeText={props.onChangeText} style={[{width:"93%",height:"100%"},t.pX4]} placeholder={props.placeholder} />
+			<TextInput 
+				secureTextEntry={showText} 
+				onChangeText={props.onChangeText} 
+				style={[{width:"93%",height:"100%"},t.pX4]} 
+				placeholder={props.placeholder} 
+				keyboardType={props.keyboardType}
+				textContentType={props.textContentType}
+				/>
+			
 			<TouchableOpacity onPressOut={()=>{iconPress(true)}} onPressIn={()=>{iconPress(false)}} >{(props.icon)?props.icon:""}</TouchableOpacity>
 		</View>
 	)
