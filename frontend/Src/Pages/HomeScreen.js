9ce62e5ba6186d6544f6a7cfb5c33275d1641a00
spynetS <alfred@stensatter.se>
2022-11-06 20:34:36 +0100

    update

--- frontend/Src/Pages/HomeScreen.js
+++ frontend/Src/Pages/HomeScreen.js
@@ -15,12 +15,14 @@ import axios from 'axios';
 export default function HomeScreen(props) {
 
 
+
   return (
    <MainView>
       <Animatable.View animation="fadeInUp" style={[t.flex,t.justifyCenter,t.itemsCenter]} >
         <View style={[]} >
           <Text style={[t.textWhite,t.text6xl]} >Hello</Text>
           <Text onPress={()=>{props.navigation.navigate("AdminCouponsPage")}} style={[t.textWhite,t.text4xl]} >{"Alla Erbjudanden"}</Text>
+          <Text onPress={()=>{global.session="undefined";props.navigation.navigate("Login")}} style={[t.textWhite,t.text2xl]} >{"Logga ut"}</Text>
         </View>
       </Animatable.View>
    </MainView>
