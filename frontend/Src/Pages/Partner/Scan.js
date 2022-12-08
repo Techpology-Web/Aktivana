74d0d9bede00e870310d0eaf49beb3ea1ea21cb1
spy <alfred@stensatter.se>
2022-12-08 23:00:45 +0100

    added example ui

--- frontend/Src/Pages/Partner/Scan.js
+++ frontend/Src/Pages/Partner/Scan.js
@@ -2,8 +2,24 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
 import {t} from "react-native-tailwindcss";
 import Menu from "../../Components/Menu";
+import InputField from "../../Components/InputField";
+import TextInputField from "../../Components/TextInputField";
+import Button from "../../Components/Button";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Scan(props){
+    const [coupon, setCoupon] = useState("")
+    const [account, setAccount] = useState("")
+
+    const use = () =>{
+        axios.post("account/useCode/",{
+            "codeId":coupon,
+            "accountId":account,
+        }).then(r=>{
+
+        })
+    }
 
     return(
         <SafeAreaView style={[{backgroundColor:"#1E1E1E",height:"100%"}]} >
@@ -14,7 +30,11 @@ export default function Scan(props){
                     ]}
                 />
                 <Text style={[t.textWhite]} >Scanner</Text>
- 
+                <View style={{display:"flex"}} >
+                    <TextInputField onChangeText={setCoupon} placeholder="coupon id" ></TextInputField>
+                    <TextInputField onChangeText={setAccount} placeholder="account id" ></TextInputField>
+                    <Button onPress={use} >Use</Button>
+                </View>
             </View>
        </SafeAreaView>
         )
